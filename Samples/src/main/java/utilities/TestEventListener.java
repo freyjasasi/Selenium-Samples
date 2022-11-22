380f770cf2167099eb639dc127da7a8c821ab78d
sasidaran.kumarasamy <freyjasasi@gmail.com>
2022-11-22 15:51:14 +0530

    minor commit

--- Samples/src/main/java/utilities/TestEventListener.java
+++ Samples/src/main/java/utilities/TestEventListener.java
@@ -1,8 +1,5 @@
 package utilities;
 
-import org.apache.commons.logging.Log;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
@@ -10,8 +7,6 @@ import org.openqa.selenium.support.events.WebDriverListener;
 import TestBase.TestBaseUtility;
 
 public class TestEventListener extends TestBaseUtility implements WebDriverListener {
-
-	private static final Logger log = LogManager.getLogger(Log.class);
 
 	@Override
 	public void beforeClick(WebElement element) {
